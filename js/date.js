bea5ba72b9f45a0b9d333c13a3890a109d99c643
ByramDigitalMedia <contact@byramdigitalmedia.com>
2025-08-07 06:51:56 -0400

    first commit

---
+++ js/date.js
@@ -0,0 +1,10 @@
+let date = new Date().getFullYear()
+document.getElementById("year").innerHTML = date
+
+// ****************************************************
+// Template Name: Sahiti Photography
+// Byram Digital Media
+// https://www.byramdigitalmedia.com
+// August 7 2025
+
+// ****************************************************
